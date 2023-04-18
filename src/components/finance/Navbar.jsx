9b2b1490ca14f6c944b9be895f894ea9c30b9814
Arno luyckx <A.luyckx@protonmail.com>
2023-04-18 13:41:52 +0200

    Minor Navbar update

--- src/components/finance/Navbar.jsx
+++ src/components/finance/Navbar.jsx
@@ -4,15 +4,18 @@ import "./Navbar.scss";
 const Navbar = () => {
   return (
     <div id="Navbar">
-      <Link to={"/"}>
-        <h2>Home</h2>
-      </Link>
-      <Link to={"/portfolio"}>
-        <h2>Portfolio</h2>
-      </Link>
-      <Link to={"/crypto"}>
-        <h2>Crypto</h2>
-      </Link>
+      <h1>Altcademy</h1>
+      <nav>
+        <Link to={"/"}>
+          <h2>Home</h2>
+        </Link>
+        <Link to={"/portfolio"}>
+          <h2>Portfolio</h2>
+        </Link>
+        <Link to={"/crypto"}>
+          <h2>Crypto</h2>
+        </Link>
+      </nav>
     </div>
   );
 };
