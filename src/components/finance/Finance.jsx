55691b95ee5f13c0b2538ab0351bf9fc7ab7c455
Arno luyckx <A.luyckx@protonmail.com>
2023-04-21 23:26:10 +0200

    ..

--- src/components/finance/Finance.jsx
+++ src/components/finance/Finance.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { getHistoricalRates, getRates } from "./api";
+import { getRates } from "./api";
 import Forex from "./Finance_Forex";
 import "./Finance_Forex.scss";
 
